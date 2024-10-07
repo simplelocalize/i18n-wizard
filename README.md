228b052a8fe99881fcd03b63d464bb9b9950e5ea
Jakub Pomykała <jakub.pomykala@gmail.com>
2024-10-07 12:30:15 +0200

    Polishing

--- README.md
+++ README.md
@@ -1,51 +1,53 @@
+# String to i18n - AI Transformer 
+
+Small CLI utility tool that helps you minizse manual effort for moving inline strings in your source code to any i18n library.
+CLI will parse your source code and move all inline strings to i18n library, 
+generate [translation keys following best practices](https://simplelocalize.io/blog/posts/best-practices-for-translation-keys/),
+and export them to a JSON file. Exported JSON file can be easily imported to [SimpleLocalize](https://simplelocalize.io), to organize and manage your translations.
+
+The CLI uses your OpenAI API key to generate translation keys. You can get your API key [here](https://platform.openai.com/account/api-keys).
+
+## Word from the author
+
+This CLI is a proof of concept and is not production-ready, thus it's a good idea to not run this on source that is not under version control,
+or to make a backup of your source code before running the CLI.
+It doesn't handle all edge cases, and the output may be different between runs, depending on the OpenAI model and the input code.
+It may require some manual adjustments after running the CLI, but it should help you minimize manual effort for moving inline strings to i18n library.
+
+Feel free to fork the repository and modify the code to fit your needs, or create a PR with new features.
+
 ## Usage
-
-![How to render image](/readme/rendering.gif)
-
-Create a JSON file in `my-directory` folder and name it `sample.json`.
-
-```json
-{
-  "renderform": true,
-  "data": [
-    {
-      "my_title.text": "My title", // 'my_title.text' is a template property from Form Editor (Preview popup)
-      "my_image.src": "https://example.com/image.png"
-    }
-  ]
-}
-```
 
 Then run the command:
 
 ```bash
-npx @renderform/cli@1.1.1 --template <YOUR_TEMPLATE> --apiKey <YOUR_API_KEY> ./my-directory
+npx @simplelocalize/string-to-i18n@1.0.0 --openAiKey <YOUR_OPEN_AI_KEY> ./my-directory/**/*.{tsx,ts}
 ```
-
-The CLI will look for all JSON files in the directory and will create a new image or PDF file for each JSON file with the same name but with the corresponding extension.
-
-![Sample output](/readme/my-blog-post.jpg)
 
 ## Options
 
-### `--template`
+### `--output`
 
-The template ID to use. You can find the template ID in the URL of the template page.
+Output file path. By default, the CLI will save the output to the `./extraction.json` file.
 
-### `--apiKey`
+### `--openAiKey`
 
-Your API key. You can find it in your RenderForm account.
+OpenAI API key. You can get your API key [here](https://platform.openai.com/account/api-keys).
+If you don't provide the key, the CLI will take it from the `OPENAI_API_KEY` environment variable.
 
-### `--overwrite`
+### `--openAiModel`
 
-Overwrite existing files. By default, the CLI skips rendering for already rendered files. Use this option to force the CLI to request the
-render and overwrite the existing file.
+OpenAI model. You can choose from `gpt-3.5-turbo` or `gpt-3.5`. Default is `gpt-3.5-turbo`.
 
-### `--debug`
+### `--generateDiff`
 
-Enable debug mode. It will print the API response in the console.
+Generate a diff file with changes made by the CLI. By default, the CLI will not generate a diff file. Default: `false`.
 
-### `--no-cache`
+### `--applyDiff`
 
-Disable cache. It will force the CLI to re-render image from the API.
+Apply the diff file to the source code. By default, the CLI will not apply the diff file. Default: `false`.
 
+
+### Customizations and modifications
+
+Feel free to fork the repository and modify the code to fit your needs, or create a PR with new features.
--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,12 +1,13 @@
 "use strict";
 import { bold, greenBright } from "colorette";
+import { generatePrompt } from "../../prompt";
 
 const fs = require("fs");
 const glob = require("glob");
-const path = require("path");
 const OpenAI = require("openai");
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
+const diff = require("diff");
 
 interface ExtractedTranslation {
   translationKey: string;
@@ -21,55 +22,64 @@ interface OpenAiAnswer {
 let client = null;
 
 const argv = yargs(hideBin(process.argv))
-  .option("apiKey")
-  .boolean("overwrite")
-  .boolean("debug").argv;
+  .string("openAiKey")
+  .string("openAiModel")
+  .string("output")
+  .boolean("generateDiff")
+  .boolean("applyDiff")
+  .argv;
+
+const outputFile = argv?.output || "extraction.json";
 
 const execute = async () => {
-  console.log("Starting extraction...");
-  const directory = argv._[argv._.length - 1];
-  if (!directory) {
-    console.log("No directory provided");
+  console.log(greenBright("Working..."));
+  const input = argv._[argv._.length - 1];
+  const example = "--input /src/**/*.{tsx,ts}";
+  if (!input) {
+    console.error(`Please provide an input directory, at the end of the command. Example: ${bold(`npx @simplelocalize/string-to-i18n --openAiKey your-key ${greenBright(example)}`)}`);
     return;
   }
 
-  if (argv.debug) {
-    console.log(bold("Directory: ") + path.resolve(directory));
-  }
-  const filePaths = glob.sync(directory + "/**/*.tsx");
-
-  const isExists = fs.existsSync("extraction.json", "utf8");
-  if (!isExists) {
-    fs.writeFileSync("extraction.json", "{}", "utf8");
+  const openAiKey = argv?.openAiKey || process.env["OPENAI_API_KEY"];
+  const generateDiff = argv?.generateDiff;
+  if (!openAiKey && generateDiff) {
+    console.error(`Please provide an OpenAI API key using ${bold(`--openAiKey`)} or by setting ${bold(`OPENAI_API_KEY`)} environment variable to generate diff patches`);
+    return;
   }
 
-  if (argv.overwrite) {
-    fs.writeFileSync("extraction.json", "{}", "utf8");
+  if (generateDiff) {
+    client = new OpenAI({
+      apiKey: argv?.openAiKey || process.env["OPENAI_API_KEY"] // This is the default and can be omitted
+    });
   }
 
-  client = new OpenAI({
-    apiKey: argv.apiKey || process.env["OPENAI_API_KEY"] // This is the default and can be omitted
-  });
+  const filePaths = glob.sync(input);
+  console.log(`Found ${bold(filePaths.length)} files to process`);
 
-  try {
-    JSON.parse(fs.readFileSync("extraction.json", "utf8"));
-  } catch (e) {
-    console.log("extraction.json is not a valid JSON file");
-  }
-
+  let counter = 1;
+  fs.writeFileSync(outputFile, "{}", "utf8");
   for (const filePath of filePaths) {
-    console.log("Processing: " + filePath);
-    await processFile(filePath);
+    console.log(`${bold(`[${counter}/${filePaths.length}] Processing:`)} ${filePath}`);
+    if (argv.generateDiff) {
+      await generateDiffWithOpenAI(filePath);
+    } else {
+      console.log(`Skipping diff generation, use ${bold(`--generateDiff`)} to generate diff patches`);
+    }
+    if (argv.applyDiff) {
+      applyDiffPatch(filePath);
+    } else {
+      console.log(`Skipping diff application, use ${bold(`--applyDiff`)} to apply diff patches`);
+    }
+    counter++;
   }
 };
 
-const processFile = async (filePath: string) => {
-  const fileWithTranslations = fs.readFileSync("extraction.json", "utf8");
+const generateDiffWithOpenAI = async (filePath: string) => {
+  const fileWithTranslations = fs.readFileSync(outputFile, "utf8");
   const translations = JSON.parse(fileWithTranslations);
-
-  const processedFileContent = await askOpenAI(filePath);
-  const diffPatch = processedFileContent?.diffPatch || "";
-  const extractedTranslationKeys = processedFileContent?.extractedTranslationKeys || [];
+  const openAiAnswer = await askOpenAI(filePath);
+  const diffPatch = openAiAnswer?.diffPatch || "";
+  const extractedTranslationKeys = openAiAnswer?.extractedTranslationKeys || [];
 
   for (let extractedTranslationKey of extractedTranslationKeys) {
     const translationKey = extractedTranslationKey.translationKey;
@@ -80,9 +90,20 @@ const processFile = async (filePath: string) => {
     };
   }
   fs.writeFileSync(filePath + ".diff", diffPatch, "utf8");
-  fs.writeFileSync("extraction.json", JSON.stringify(translations, null, 2), "utf8");
+  fs.writeFileSync(outputFile, JSON.stringify(translations, null, 2), "utf8");
 };
 
+const applyDiffPatch = (filePath: string) => {
+  const diffFilePath = filePath + ".diff";
+  if (!fs.existsSync(diffFilePath)) {
+    return;
+  }
+  const diffPatch = fs.readFileSync(diffFilePath, "utf8");
+  const fileContent = fs.readFileSync(filePath, "utf8");
+  const patches = diff.parsePatch(diffPatch);
+  const patchedContent = diff.applyPatch(fileContent, patches);
+  fs.writeFileSync(filePath, patchedContent, "utf8");
+};
 
 interface OpenAiExtractionItem {
   k: string,
@@ -95,26 +116,27 @@ interface OpenAiRawAnswer {
 }
 
 const askOpenAI = async (filePath: string): Promise<OpenAiAnswer> => {
-  const fileContent = fs.readFileSync(filePath, "utf8");
 
-  const content = `
-  You are a developer working on a React project that needs to be translated into multiple languages. You have a source code file that is at ${filePath} with the following content:
-  ${fileContent}
-  
-  Find all texts in the source code that should be translated and provide the translation key and the default message.
-  If the source code file is already in the correct format, you can respond with an empty diff patch and an empty list of extracted translations.
-  Use FormatJS libraries to handle translations.
-  Response format:
-  - Provide a diff patch with the changes needed to extract the translations.
-  - Provide a list of extracted translations with the translation key and the default message.
-  Response as a minified valid JSON that looks like this: {"d": "diff patch", "e": [{"k": "translation key", "m": "default message"}]}
-  `;
+  const fileContent = fs.readFileSync(filePath, "utf8");
+  const content = generatePrompt({
+    filePath,
+    fileContent
+  });
 
   const result = await client.chat.completions.create({
-    messages: [{ role: "user", content }],
-    model: "gpt-3.5-turbo"
+    messages: [
+      {
+        role: "user",
+        content
+      }
+    ],
+    model: argv?.openAiModel || "gpt-4o",
   });
+
+  console.log(result);
+
   const response = result.choices[0].message.content;
+  console.log(response);
   const answer = JSON.parse(response) as OpenAiRawAnswer;
   return {
     diffPatch: answer.d,
@@ -128,7 +150,7 @@ const askOpenAI = async (filePath: string): Promise<OpenAiAnswer> => {
 (async () => {
   try {
     await execute();
-    console.log(greenBright("Finished!"));
+    console.log(greenBright("Done!"));
     process.exit(0);
   } catch (e) {
     console.log("");
